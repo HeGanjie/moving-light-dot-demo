3178a2764989cdb95a2f948c7c145981c15d0d57
heganjie <heganj@qq.com>
2019-11-10 13:53:55 +0800

    successful import react-regl

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Regl, { Draw } from 'react-regl';
+import './App.css'
+
+let vertShader = `
+precision mediump float;
+attribute vec2 position;
+void main() {
+  gl_Position = vec4(position, 0, 1);
+}`
+let fragShader = `
+precision mediump float;
+uniform vec4 color;
+void main() {
+  gl_FragColor = color;
+}`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Regl
+      width={window.innerWidth}
+      height={window.innerHeight}
+      color={[0, 0, 0, 1]}
+    >
+      <Draw
+        vert={vertShader}
+        frag={fragShader}
+        attributes={{
+          position: [[-0.5, 0],[0, -0.5],[0.25, 1]]
+        }}
+        uniforms={{
+          color: [1,1,0.5,1]
+        }}
+        count={3}
+      />
+    </Regl >
   );
 }
 
